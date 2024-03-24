fcd5d4624bd20f457254b337654509aff9a59d7c
Calvin Chan <xlysergia@gmail.com>
2024-03-24 11:02:02 +0000

    bg

--- framesjs-starter/app/api/generate-main/route.tsx
+++ framesjs-starter/app/api/generate-main/route.tsx
@@ -44,8 +44,8 @@ const handleRequest = frames(async (ctx) => {
 
   return {
     image: (
-      <div tw="flex w-full h-full bg-gray-900 p-3">
-<span tw="bg-blue-500 text-white p-4 rounded-lg">
+      <div tw="flex w-full h-full bg-teal-800 p-3">
+<span tw="bg-gray-900 text-white p-4 rounded-lg">
     <div tw="flex flex-col">
       <div tw="flex bg-blue-700 text-white p-2 rounded-md justify-between items-center">
         <div tw="flex w-1/2">
