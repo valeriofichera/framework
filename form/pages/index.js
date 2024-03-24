334ba10fd6c1a2b4cf0719a998e61dcfd3526f5a
Valerio <78494380+valeriofichera@users.noreply.github.com>
2024-03-24 03:02:12 +0100

    ADDED CHART

--- form/pages/index.js
+++ form/pages/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import FormComponent from '@/components/Form.js'
+import LineChartComponent from '@/components/chart.js'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <FormComponent/>
+      <LineChartComponent/>
     </main>
   );
 }
